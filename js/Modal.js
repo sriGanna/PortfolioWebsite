b29c64d9fd2ac62565e31af25e45e996eea5e770
sgannav <sri.ganna@gmail.com>
2018-09-17 10:48:07 -0400

    Original Commit
    Original Commit, website HTML, CSS and JS and content(images)

---
+++ js/Modal.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myImg");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "block";
+}
